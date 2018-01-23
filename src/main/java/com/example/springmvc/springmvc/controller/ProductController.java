251d02ba8ccf48e0dcf001ef7bcaec4b52f4e018
melo <mmashabane190291@gmail.com>
2018-01-23 07:28:37 +0000

    Create Product Page

--- src/main/java/com/example/springmvc/springmvc/controller/ProductController.java
+++ src/main/java/com/example/springmvc/springmvc/controller/ProductController.java
@@ -1,7 +1,12 @@
 package com.example.springmvc.springmvc.controller;
 
+import com.example.springmvc.springmvc.model.Product;
+import com.example.springmvc.springmvc.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by mpumelelomashabane on 15/01/2018.
@@ -10,9 +15,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProductController {
 
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @RequestMapping(path = "/")
     public String index(){
         return "index";
+    }
+
+    @RequestMapping(path = "/products/add", method = RequestMethod.GET)
+    public String createProdct(Model model){
+     model.addAttribute("product", new Product());
+     return "edit";
+    }
+
+    @RequestMapping(path = "products", method = RequestMethod.POST)
+    public String saveProduct(Product product){
+        productRepository.save(product);
+        return "redirect:/";
     }
 
 }
